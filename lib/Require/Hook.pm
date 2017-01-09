540baff67d80ecbc69753249102918bef6e8ae49
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-09 19:02:08 +0700

    Initial commit

---
+++ lib/Require/Hook.pm
@@ -0,0 +1,16 @@
+package Require::Hook;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Base class for require() hook class
+
+=head1 DESCRIPTION
+
+Currently empty, name grab only.
+
+
+=head1 SEE ALSO
+
+C<Require::Hook::*> modules.
